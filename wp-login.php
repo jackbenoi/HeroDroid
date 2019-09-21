998e59d8e45eb96f8c48bb4e27423f6cb3fc0144
jackbenoi <55368490+jackbenoi@users.noreply.github.com>
2019-09-21 18:50:07 +0100

    new website

---
+++ wp-login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="fr-FR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="fr-FR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Se connecter &lsaquo; HeroDroid &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://herodroid.com/wp-includes/css/dashicons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://herodroid.com/wp-includes/css/buttons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://herodroid.com/wp-admin/css/forms.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://herodroid.com/wp-admin/css/l10n.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://herodroid.com/wp-admin/css/login.min.css?ver=5.2.3' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-fr-fr">
+		<div id="login">
+		<h1><a href="https://fr.wordpress.org/">Propulsé par WordPress</a></h1>
+	
+	<form name="loginform" id="loginform" action="https://herodroid.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Identifiant ou adresse e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Mot de passe<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Se souvenir de moi</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter" />
+				<input type="hidden" name="redirect_to" value="https://herodroid.com/wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+					<a href="https://herodroid.com/wp-login.php?action=lostpassword">Mot de passe oublié ?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="https://herodroid.com/">
+		&larr; Retour vers HeroDroid	</a></p>
+			
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
